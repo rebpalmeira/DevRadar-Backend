d4c791a7e0bf9d6840fc427e1871072af161de6d
Lucas palmeira <rebekamaisa@gmail.com>
2020-01-18 20:34:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+mongoose.connect('mongodb+srv://omnistack:@Aa456963456@cluster0-j221r.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+app.use(express.json());
+app.use(routes);
+app.listen(3333);

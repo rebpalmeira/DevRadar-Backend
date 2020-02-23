4f179fea7b650d2e52f414180c2321161a4be6e6
Rebeka Palmeira <rebekamaisa@gmail.com>
2020-02-23 16:53:39 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 mongoose.connect('mongodb+srv://omnistack:@Aa456963456@cluster0-j221r.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
+
